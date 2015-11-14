fb32daf87399699ac83739a360d75fe00e16a41f
joeypark <joeypark84@gmail.com>
2015-11-14 21:44:31 +0900

    feat : 기본 서버 통신 구현(이름 주고 받기)

---
+++ Assets/Scripts/sample.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class sample : MonoBehaviour {
+
+    public InputField nameField;
+    public Text logField;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnBtnOK()
+    {
+        StartCoroutine(coNameEnter());
+    }
+
+    IEnumerator coNameEnter()
+    {
+        string inputNameURL = string.Format("http://localhost/userNameAction.php?name={0}", nameField.text);
+        inputNameURL = inputNameURL.Replace(" ", "+");
+        WWW www = new WWW(inputNameURL);
+        yield return www;
+
+        logField.text = www.text;
+    }
+}
